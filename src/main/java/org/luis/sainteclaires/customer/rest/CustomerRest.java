26fafeff5b5f53782659ba4b3902ca6843fd8ce3
Li, Guoliang <kxmd2008@gmail.com>
2014-06-17 15:57:36 +0800

    udpate files

---
+++ src/main/java/org/luis/sainteclaires/customer/rest/CustomerRest.java
@@ -0,0 +1,93 @@
+package org.luis.sainteclaires.customer.rest;
+
+import org.luis.basic.rest.model.SimpleMessage;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class CustomerRest {
+
+	@RequestMapping(value = "login", method = RequestMethod.POST)
+	@ResponseBody
+	public SimpleMessage login(String loginName, String password) {
+		SimpleMessage sm = new SimpleMessage();
+
+		return sm;
+	}
+
+	@RequestMapping(value = "logout", method = RequestMethod.POST)
+	@ResponseBody
+	public SimpleMessage logout(String loginName) {
+		SimpleMessage sm = new SimpleMessage();
+
+		return sm;
+	}
+
+	@RequestMapping(value = "changePassword", method = RequestMethod.POST)
+	@ResponseBody
+	public SimpleMessage changePassword(String oldPwd, String newPwd,
+			String confirmPwd) {
+		SimpleMessage sm = new SimpleMessage();
+
+		return sm;
+	}
+
+	@RequestMapping(value = "bag/addItem/{shotId}/{itemId}", method = RequestMethod.GET)
+	@ResponseBody
+	public SimpleMessage addProductToBag(@PathVariable("shotId") String shotId,
+			@PathVariable("itemId") String itemId) {
+		SimpleMessage sm = new SimpleMessage();
+
+		return sm;
+	}
+
+	@RequestMapping(value = "bag/editItem/{itemId}/{num}", method = RequestMethod.GET)
+	@ResponseBody
+	public SimpleMessage editItem(@PathVariable("itemId") String itemId,
+			@PathVariable("num") int num) {
+		SimpleMessage sm = new SimpleMessage();
+
+		return sm;
+	}
+
+	@RequestMapping(value = "bag/deleteItem/{shotId}/{itemId}", method = RequestMethod.GET)
+	@ResponseBody
+	public SimpleMessage deleteItem(@PathVariable("shotId") String shotId,
+			@PathVariable("itemId") String itemId) {
+		SimpleMessage sm = new SimpleMessage();
+
+		return sm;
+	}
+
+	@RequestMapping(value = "createOrder/{shotId}", method = RequestMethod.GET)
+	@ResponseBody
+	public SimpleMessage createOrder(@PathVariable("shotId") String shotId) {
+		SimpleMessage sm = new SimpleMessage();
+
+		return sm;
+	}
+
+	@RequestMapping(value = "order/editItem/{itemId}/{num}", method = RequestMethod.GET)
+	@ResponseBody
+	public SimpleMessage editItemInOrder(@PathVariable("itemId") String itemId,
+			@PathVariable("num") int num) {
+		SimpleMessage sm = new SimpleMessage();
+
+		return sm;
+	}
+
+	@RequestMapping(value = "order/deleteItem/{orderId}/{itemId}", method = RequestMethod.GET)
+	@ResponseBody
+	public SimpleMessage deleteItemInOrder(
+			@PathVariable("orderId") String orderId,
+			@PathVariable("itemId") String itemId) {
+		SimpleMessage sm = new SimpleMessage();
+
+		return sm;
+	}
+
+}
